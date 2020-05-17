60d9187e153619394b873ebbddf9217170cc9ef5
claryfray <targaryenceleste@gmail.com>
2020-05-17 21:34:59 +1000

    serializers

--- interviews/views.py
+++ interviews/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import PersonalInfo, Interviewer, Interviewee, InterviewSlot, InterviewData
+from .serializers import PersonalInfoSerializer, InterviewerSerializer, IntervieweeSerializer, InterviewSlotSerializer, \
+    InterviewDataSerializer
+
 
 # Create your views here.
+
+class ListPersonalInfo(generics.ListCreateAPIView):
+    queryset = PersonalInfo.objects.all()
+    serializer_class = PersonalInfoSerializer
+
+
+class DetailPersonalInfo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PersonalInfo.objects.all()
+    serializer_class = PersonalInfoSerializer
+
+
+class ListInterviewer(generics.ListCreateAPIView):
+    queryset = Interviewer.objects.all()
+    serializer_class = InterviewerSerializer
+
+
+class DetailInterviewer(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Interviewer.objects.all()
+    serializer_class = InterviewerSerializer
+
+
+class ListInterviewee(generics.ListCreateAPIView):
+    queryset = Interviewee.objects.all()
+    serializer_class = IntervieweeSerializer
+
+
+class DetailInterviewee(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Interviewee.objects.all()
+    serializer_class = IntervieweeSerializer
+
+
+class ListInterviewSlot(generics.ListCreateAPIView):
+    queryset = InterviewSlot.objects.all()
+    serializer_class = InterviewSlotSerializer
+
+
+class DetailInterviewSlot(generics.RetrieveUpdateDestroyAPIView):
+    queryset = InterviewSlot.objects.all()
+    serializer_class = InterviewSlotSerializer
+
+
+class ListInterviewData(generics.ListCreateAPIView):
+    queryset = InterviewData.objects.all()
+    serializer_class = InterviewDataSerializer
+
+
+class DetailInterviewData(generics.RetrieveUpdateDestroyAPIView):
+    queryset = InterviewData.objects.all()
+    serializer_class = InterviewDataSerializer
