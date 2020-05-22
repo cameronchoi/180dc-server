6d65317f8b0ed3efe73381d0f25f402172ebf8f6
sap1l <30491887+sap1l@users.noreply.github.com>
2020-05-22 17:54:03 +1000

    added interviewer/interviewee serializers

--- interviews/serializers.py
+++ interviews/serializers.py
@@ -1,11 +1,23 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import PersonalInfo, Interviewer, Interviewee, InterviewSlot, InterviewData
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = [
+            'username',
+            'password',
+            'first_name',
+            'last_name',
+            'email',
+        ]
+        model = User
 
 
 class PersonalInfoSerializer(serializers.ModelSerializer):
     class Meta:
         fields = (
-            'id',
             'degree_one',
             'degree_two',
             'major_one',
@@ -14,26 +26,47 @@ class PersonalInfoSerializer(serializers.ModelSerializer):
         model = PersonalInfo
 
 
+class IntervieweeSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    personal_info = PersonalInfoSerializer()
+
+    class Meta:
+        fields = [
+            'user',
+            'personal_info',
+            'digital_impact',
+            'previous_score',
+        ]
+        model = Interviewee
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        personal_info_data = validated_data.pop('personal_info')
+        user = User.objects.create(**user_data)
+        personal_info = PersonalInfo.objects.create(**personal_info_data)
+        interviewee = Interviewee.objects.create(user=user, personal_info=personal_info, **validated_data)
+        return interviewee
+
+
 class InterviewerSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    personal_info = PersonalInfoSerializer()
+
     class Meta:
         fields = (
-            'id',
             'user',
             'personal_info',
             'digital_impact',
         )
         model = Interviewer
 
-
-class IntervieweeSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = (
-            'user',
-            'personal_info',
-            'digital_impact',
-            'previous_score',
-        )
-        model = Interviewee
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        personal_info_data = validated_data.pop('personal_info')
+        user = User.objects.create(**user_data)
+        personal_info = PersonalInfo.objects.create(**personal_info_data)
+        interviewer = Interviewer.objects.create(user=user, personal_info=personal_info, **validated_data)
+        return interviewer
 
 
 class InterviewSlotSerializer(serializers.ModelSerializer):
