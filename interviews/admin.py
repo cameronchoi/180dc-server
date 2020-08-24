ae72f92b8639c096a1b4cf29e7fdcca416032539
sap1l <30491887+sap1l@users.noreply.github.com>
2020-08-24 17:46:49 +1000

    added options to admin panel

--- interviews/admin.py
+++ interviews/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Interviewer, Interviewee, InterviewData
+from .models import Options, Interviewer, Interviewee, InterviewData
 
 # Register your models here.
+admin.site.register(Options)
 admin.site.register(Interviewer)
 admin.site.register(Interviewee)
 admin.site.register(InterviewData)
