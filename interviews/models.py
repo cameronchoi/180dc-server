9294899179b68dbabe8651349dd5a88467985cf4
sap1l <30491887+sap1l@users.noreply.github.com>
2020-08-24 23:36:05 +1000

    temp allow max interviews to be blank before deletion

--- interviews/models.py
+++ interviews/models.py
@@ -21,7 +21,7 @@ class Interviewer(models.Model):
     degree_two = models.CharField(max_length=150, blank=True)
     major_one = models.CharField(max_length=150, blank=True)
     major_two = models.CharField(max_length=150, blank=True)
-    max_interviews = models.IntegerField()
+    max_interviews = models.IntegerField(blank=True, null=True)
     digital_impact = models.BooleanField(default=False)  # false = strategy
 
     class Meta:
