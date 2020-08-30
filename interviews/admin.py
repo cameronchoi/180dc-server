0bab7b016376f2d942a8d7dc0e51d98a8dd0a9a0
Cameron Choi <cameron.choi99@gmail.com>
2020-08-30 13:48:51 +1000

    allow download all csv

--- interviews/admin.py
+++ interviews/admin.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 
 from .models import Option, Interviewer, Interviewee, InterviewData
 from .management.commands.generate_interview_schedule import generate_interview_data_df
+from .management.commands.generate_interview_schedule_second import generate_interview_data_second
 
 
 def download_interview_date(modeladmin, request, queryset):
@@ -29,11 +30,36 @@ def download_interview_date(modeladmin, request, queryset):
     return response
 
 
-download_interview_date.short_description = "Download all interview slots as a csv"
+download_interview_date.short_description = "Download SOME interview slots which contain interviewees as a csv"
+
+
+def download_interview_date_second(modeladmin, request, queryset):
+    interview_data_df = generate_interview_data_second()
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="interviews.csv"'
+
+    writer = csv.writer(response)
+    header_row = [
+        'Date',
+        'Time',
+        'Room',
+        'Digital Impact',
+        'Interviewers',
+        'Interviewees'
+    ]
+    writer.writerow(header_row)
+
+    for row in interview_data_df.values.tolist():
+        writer.writerow(row)
+
+    return response
+
+
+download_interview_date_second.short_description = "Download ALL interview slots as a csv"
 
 
 class InterviewDataAdmin(admin.ModelAdmin):
-    actions = [download_interview_date]
+    actions = [download_interview_date, download_interview_date_second]
 
 
 # Register your models here.
---
+++ interviews/management/commands/generate_interview_schedule_second.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from interviews.models import InterviewData
+
+
+def generate_interview_data_second():
+    # create dataframe
+    df = pd.DataFrame(columns=[
+        'Date',
+        'Time',
+        'Room',
+        'Digital Impact',
+        'Interviewers',
+        'Interviewees'
+    ])
+
+    # iterate thru db to process interviews
+    for interview_data in InterviewData.objects.all():
+        # generate interviewee list (of their full names)
+        interviewees = []
+        for interviewee in interview_data.interviewees.all():
+            full_name = "{} {}".format(
+                interviewee.user.first_name, interviewee.user.last_name)
+            interviewees.append(full_name)
+
+        # generate interviewer list (of their full names)
+        interviewers = []
+        for interviewer in interview_data.interviewers.all():
+            full_name = "{} {}".format(
+                interviewer.user.first_name, interviewer.user.last_name)
+            interviewers.append(full_name)
+
+        # append to the dataframe
+        # we don't need to check for unique
+        # because that constraint is assumed to be true on a db level
+        df = df.append(
+            {
+                'Date': interview_data.datetime.date(),
+                'Time': interview_data.datetime.time(),
+                'Room': interview_data.room,
+                'Digital Impact': interview_data.digital_impact,
+                'Interviewers': (interviewers if interviewers else None),
+                'Interviewees': (interviewees if interviewees else None)
+            },
+            ignore_index=True)
+
+    return df
+
+
+class Command(BaseCommand):
+    help = "generates current interview schedule"
+
+    def handle(self, *args, **options):
+        interview_data_df = generate_interview_data_second()
+
+        # finally output to csv
+        output_file = os.path.join(
+            settings.BASE_DIR, "interviews/csv-files/interviews.csv")
+        interview_data_df.to_csv(output_file, index=False)
+
+        self.stdout.write("success!")
